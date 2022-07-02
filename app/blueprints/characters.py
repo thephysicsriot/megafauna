251988ce06d2428d42730b1875334635f5950857
Kristen Zych <kristenzych@gmail.com>
2022-07-01 20:02:04 -0400

    feat: Adding character views for get, post, patch

--- app/blueprints/characters.py
+++ app/blueprints/characters.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
-from app.views.characters import ListCharactersView
+from app.views.characters import ListCharactersView, GetCharacterView
 
 
 CharactersBlueprint = Blueprint('characters', __name__, url_prefix='/characters')
 
 CharactersBlueprint.add_url_rule('/', view_func=ListCharactersView.as_view('list-characters'))
+CharactersBlueprint.add_url_rule('/<pk>', view_func=GetCharacterView.as_view('get-character'))
--- app/views/characters.py
+++ app/views/characters.py
@@ -1,13 +1,35 @@
-from flask import jsonify
+from flask import jsonify, request
 from flask.views import MethodView
 from app.views.auth import login_required
 from app.models.character import Character
+
 
 class ListCharactersView(MethodView):
 
     @login_required
     def get(self):
-      characters = Character.objects().all()
-      characters = [c.to_json() for c in characters]
-      return jsonify(characters)
+        characters = Character.objects().all()
+        characters = [c.to_json() for c in characters]
+        return jsonify(characters)
 
+    @login_required
+    def post(self):
+        json = request.get_json()
+        character = Character(**json)
+        character.save()
+        return jsonify(character.to_json())
+
+
+class GetCharacterView(MethodView):
+
+    @login_required
+    def get(self, pk):
+        character = Character.objects(id=pk).first()
+        return jsonify(character.to_json())
+
+    @login_required
+    def patch(self, pk):
+        json = request.get_json()
+        character = Character.objects(id=pk).first()
+        character.modify(**json)
+        return jsonify(character.to_json())
